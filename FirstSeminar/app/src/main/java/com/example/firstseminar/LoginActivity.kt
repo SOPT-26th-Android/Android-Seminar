55836c4322a15f7281759d61916396c25a05240b
CHJUNE-android <chjune0205@gmail.com>
2020-03-17 16:28:40 +0900

    [1차 세미나 내용 업로드]

---
+++ FirstSeminar/app/src/main/java/com/example/firstseminar/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.firstseminar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener {
+            if(et_id.text.isNullOrBlank()||et_password.text.isNullOrBlank()){
+                Toast.makeText(this,"아이디와 비밀번호를 확인하세요",Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        tv_register.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- FirstSeminar/app/src/main/java/com/example/firstseminar/RegisterActivity.kt
+++ FirstSeminar/app/src/main/java/com/example/firstseminar/RegisterActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.firstseminar.R
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
